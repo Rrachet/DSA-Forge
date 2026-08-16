1ddf1c5820908cb69b59c9b7ca56057bb362c5a8
Rrachet <136880232+Rrachet@users.noreply.github.com>
2026-08-16 20:32:07 +0530

    feat: add shared binary tree node

---
+++ src/main/java/com/rrachet/dsaforge/trees/TreeNode.java
@@ -0,0 +1,9 @@
+package com.rrachet.dsaforge.trees;
+
+/** Simple binary-tree node shared by tree algorithms. */
+public final class TreeNode {
+    public final int value;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int value) { this.value = value; }
+}
